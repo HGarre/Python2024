8afccf89c98733be7d00516b300d936b8f8558fc
HGarre <helene@garre.eu>
2024-04-23 14:45:30 +0200

    corrected variable selection, added comments

--- Assignment_5_6_converting_formulas.py
+++ Assignment_5_6_converting_formulas.py
@@ -44,33 +44,33 @@ The function is free to choose names for the variables.
 import re
 
 def take_apart_formula(formula):
-    pattern = '\([^()]+\)'
-    variables = 'pqrstovw'
-    indices = 'abcdefghijklmn'
+    pattern = '\([^()]+\)' #matches one or more (+) characters that are not (^) brackets (\ to escape the special character) between brackets
+    variables = 'pqrstovw' #possible main names for the variables that are created
+    indices = 'abcdefghijklmn' #indices for the variables to be created (limitation: doubles if more than 14 terms in brackets need to be substituted)
     i = 0
     set = False
-    while i < len(variables) and set == False:
-        if indices[i] in formula:
+    while i < len(variables)-1 and set == False: #loop through all possible variable names until a decition is made (set = true)
+        if variables[i] not in formula: # if the current variable name is not used in the formula, pick it and stop the loop
             var = variables[i]
             set = True
-        else:
+        else: # else try the next possible variable name
             i += 1
-    if not set:
+    if not set: # if (very improbale but possible) all variables 'pqrstovw' are used in the formula, groupa, groupb, etc is used instead
         var = 'group'
-    m = re.search(pattern, formula)
+    m = re.search(pattern, formula) #find innermost expression
     list = []
     ind = 0
     while m != None:
-        found = m.group()
-        substitution = var + indices[ind] + ' = ' + found[1:-1]
-        list += [substitution]
-        formula = formula[:m.start()] + var+indices[ind] + formula[m.end():]
-        m = re.search(pattern, formula)
+        found = m.group() #extract the expression
+        substitution = var + indices[ind] + ' = ' + found[1:-1] #construct a string that states the new variable equal to this expression (without the flanking brackets)
+        list += [substitution] #add it to the list
+        formula = formula[:m.start()] + var+indices[ind] + formula[m.end():] #update the formula with the new variable between the start and end of the expression
+        m = re.search(pattern, formula) #find the new innermost expression
         ind += 1
-    list += [formula]
+    list += [formula] #add the remaining formula to the list
     return list
 
-string = 'a+(3-q)/(y*(4-x)*(2+a))'
+string = 'a+(3-p)/(y*(4-x)*(2+a))'
 
 print(take_apart_formula(string))
 # output: ['pa = 3-q', 'pb = 4-x', 'pc = 2+a', 'pd = y*pb*pc', 'a+pa/pd']
